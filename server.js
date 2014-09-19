2c4bbb86d61fdb3207660071d698ec5e6cd13b94
Daniel van Hoesel <daniel@danielvanhoesel.nl>
2014-09-19 16:41:51 +0200

    Add annoying speaker sounds

--- server.js
+++ server.js
@@ -30,14 +30,32 @@ if (process.env.NODE_ENV == "production") {
   var Gpio = require("onoff").Gpio;
   var left = new Gpio(17, "in", "rising");
   var right = new Gpio(18, "in", "rising");
+  var speaker = new Gpio(22, "out");
+  var sleep = require("sleep");
+
+  var yeah = function(pitch) {
+    var duration = 0.15;
+    var period = 1.0 / pitch;
+    var delay = period / 2;
+    var cycles = duration * pitch | 0;
+
+    for (var i = 0; i < cycles; i++) {
+      speaker.writeSync(1);
+      sleep(delay * 1000000);
+      speaker.writeSync(0);
+      sleep(delay * 1000000);
+    }
+  }
 
   left.watch(function(err, signal) {
     if (err) return;
     game.score("left");
+    yeah(200);
   });
 
   right.watch(function(err, signal) {
     if (err) return;
     game.score("right");
+    yeah(400);
   });
 }
