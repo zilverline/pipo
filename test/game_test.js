7ba69c9d9e1ba899b1334559881f3c5bcb476d26
Bart Bakker <bbakker@zilverline.com>
2014-09-26 16:41:03 +0200

    Test scoring of a player

--- test/game_test.js
+++ test/game_test.js
@@ -1,7 +1,19 @@
 var expect = require('chai').expect;
+var Game = require('./../lib/game');
 
 describe('Game', function() {
-  it ('should run a mocha test', function(done) {
-    done();
+  it ('should start with empty scores', function() {
+    var events = {};
+    var game = Game(events);
+    expect(game.currentState().players['left'].score).to.equal(0);
+    expect(game.currentState().players['right'].score).to.equal(0);
+  });
+
+  it ('should increase score as player scores', function() {
+    var events = { emit: function() {} };
+    var game = Game(events);
+    game.score('left');
+    expect(game.currentState().players['left'].score).to.equal(1);
+    expect(game.currentState().players['right'].score).to.equal(0);
   });
 });
