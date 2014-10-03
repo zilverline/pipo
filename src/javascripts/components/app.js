15018db4a437fa869a33b9ec457ca8d6ce3ad87c
Daniel van Hoesel <daniel@danielvanhoesel.nl>
2014-10-03 11:27:02 +0200

    Transition start screen to game screen

--- src/javascripts/components/app.js
+++ src/javascripts/components/app.js
@@ -1,15 +1,24 @@
 /** @jsx React.DOM */
 
-var React = require("react");
+var React = require("react/addons");
 var StartGameComponent = require("./start_game");
 var ScoreBoard = require("./score_board");
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 module.exports = React.createClass({
   render: function () {
+    return (
+      <ReactCSSTransitionGroup transitionName="page">
+        {this.renderGameState()}
+      </ReactCSSTransitionGroup>
+    );
+  },
+
+  renderGameState: function() {
     if (this.props.game.status === "idle") {
-      return <StartGameComponent />
+      return <StartGameComponent key="start" />
     } else {
-      return <ScoreBoard game={this.props.game} />
+      return <ScoreBoard key="board" game={this.props.game} />
     }
   }
 });
